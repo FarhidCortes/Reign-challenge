e2456a35d74e2e4b43d48591976d5f5256c630e2
FarhidCortes <farhidcorteszambra@gmail.com>
2022-08-03 00:53:58 -0400

    Database configuration and routes test files added

--- src/controllers/newsController.js
+++ src/controllers/newsController.js
@@ -4,9 +4,19 @@ const { response, request } = require('express');
 
 //when "/news" is called
 const getNews = async (req = request, res = response) => {
+
+
+    const { limit = 5, from = 0 } = req.query;
+
+    const [ news ] = await Promise.all([
+        News.find()
+            .skip( Number( from ) )
+            .limit(Number( limit ))
+    ]);
+
     return res.json({
-        'msg':'hello world'
-    })
+        news
+    });
 }
 
 module.exports = {
--- src/models/server.js
+++ src/models/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-//const {dbConnection} = require('../database/config');
+const { dbConnection } = require('../database/config');
 
 class Server {
     constructor(){
@@ -20,7 +20,7 @@ class Server {
     }
 
     connectDB(){
-        //dbConnection();
+        dbConnection();
     }
 
     middlewares(){
